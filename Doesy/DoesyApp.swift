3e9aec9350c62e02fe6d3aa951f8e77820ce4024
mfsaglam <mfsaglam1@gmail.com>
2021-10-16 16:24:04 +0300

    Initial Commit

---
+++ Doesy/DoesyApp.swift
@@ -0,0 +1,17 @@
+//
+//  DoesyApp.swift
+//  Doesy
+//
+//  Created by Fatih Sağlam on 16.10.2021.
+//
+
+import SwiftUI
+
+@main
+struct DoesyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
