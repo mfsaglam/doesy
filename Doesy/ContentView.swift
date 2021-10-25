d0572b8ed17c415dff235a0a209f9f4db0e5edd8
mfsaglam <mfsaglam1@gmail.com>
2021-10-25 16:34:58 +0300

    Create subviews and adjust shadows for scrollviews.

--- Doesy/ContentView.swift
+++ Doesy/ContentView.swift
@@ -13,44 +13,61 @@ struct ContentView: View {
             Color(#colorLiteral(red: 0.9811814427, green: 0.9878709912, blue: 1, alpha: 1))
                 .ignoresSafeArea()
             VStack(alignment: .leading) {
-                Text("What's up, Joy!")
-                    .font(.largeTitle)
-                    .bold()
                 Text("CATEGORIES")
                     .font(.system(.footnote))
                     .opacity(0.4)
+                    .padding()
+                    .padding(.top)
                 ScrollView(.horizontal, showsIndicators: false) {
                     HStack(spacing: 10) {
                         ForEach(0 ..< 5) { item in
-                            VStack(alignment: .leading,spacing: 10) {
-                                Text("40 tasks")
-                                    .font(.caption)
-                                    .opacity(0.4)
-                                Text("Business")
-                                    .font(.title2)
-                                    .bold()
-                                Capsule(style: .continuous)
-                                    .fill(Color.secondary)
-                                    .opacity(0.3)
-                                    .frame(width: 180, height: 3)
-                                    .overlay(
-                                        HStack {
-                                            Capsule(style: .continuous)
-                                                .fill(Color(#colorLiteral(red: 0.8735848069, green: 0.2416511774, blue: 0.912528336, alpha: 1)))
-                                            .frame(width: 100)
-                                            .shadow(color: Color(#colorLiteral(red: 0.8735848069, green: 0.2416511774, blue: 0.912528336, alpha: 1)).opacity(0.4), radius: 5)
-                                            Spacer()
-                                        }
-                                    )
-                            }
-                            .padding()
-                            .background(Color.white)
-                            .clipShape(RoundedRectangle(cornerRadius: 20, style: .continuous))
-                            .shadow(color: .black.opacity(0.2), radius: 40, x: 0, y: 20)
+                            CardView()
+                                .shadow(color: .black.opacity(0.03), radius: 8, x: 0, y: 4)
                         }
                     }
                     .padding()
                 }
+                Text("TODAY'S TASKS")
+                    .font(.system(.footnote))
+                    .opacity(0.4)
+                    .padding()
+                ScrollView(showsIndicators: false) {
+                    ForEach(0 ..< 5) { item in
+                        RowView()
+                            .shadow(color: .black.opacity(0.03), radius: 8, x: 0, y: 4)
+                    }
+                }
+                .padding()
+            }
+            HStack {
+                Spacer()
+                VStack {
+                    Spacer()
+                    Image(systemName: "plus")
+                        .font(.title)
+                        .padding()
+                        .background(Color.blue)
+                        .foregroundColor(.white)
+                        .clipShape(Circle())
+                        .shadow(color: .blue.opacity(0.3), radius: 10, y: 10)
+                }
+                .padding(.trailing)
+            }
+        }
+        .navigationTitle("What's up, Joy!")
+        .toolbar {
+            ToolbarItem(placement: .navigationBarLeading) {
+                Image(systemName: "line.3.horizontal")
+                    .opacity(0.5)
+            }
+            ToolbarItem(placement: .navigationBarTrailing) {
+                Image(systemName: "magnifyingglass")
+                    .padding()
+                    .opacity(0.5)
+            }
+            ToolbarItem(placement: .navigationBarTrailing) {
+                Image(systemName: "bell")
+                    .opacity(0.5)
             }
         }
     }
@@ -58,7 +75,57 @@ struct ContentView: View {
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        NavigationView {
+            ContentView()
+        }
     }
 }
 
+
+struct CardView: View {
+    var body: some View {
+        VStack(alignment: .leading,spacing: 10) {
+            Spacer()
+            Text("40 tasks")
+                .font(.caption)
+                .opacity(0.4)
+            Text("Business")
+                .font(.title2)
+                .bold()
+            Capsule(style: .continuous)
+                .fill(Color.secondary)
+                .opacity(0.3)
+                .frame(width: 180, height: 3)
+                .overlay(
+                    HStack {
+                        Capsule(style: .continuous)
+                            .fill(Color(#colorLiteral(red: 0.8735848069, green: 0.2416511774, blue: 0.912528336, alpha: 1)))
+                            .frame(width: 100)
+                            .shadow(color: Color(#colorLiteral(red: 0.8735848069, green: 0.2416511774, blue: 0.912528336, alpha: 1)).opacity(0.4), radius: 5)
+                        Spacer()
+                    }
+                )
+        }
+        .frame(maxHeight: 80)
+        .padding()
+        .padding(.vertical, 15)
+        .background(Color.white)
+        .clipShape(RoundedRectangle(cornerRadius: 20, style: .continuous))
+    }
+}
+
+struct RowView: View {
+    var body: some View {
+        HStack {
+            Image(systemName: "circle")
+                .foregroundColor(Color(#colorLiteral(red: 0.8735848069, green: 0.2416511774, blue: 0.912528336, alpha: 1)))
+            Text("Daily meeting with team")
+                .opacity(0.7)
+            Spacer()
+        }
+        .padding()
+        .padding(.vertical, 8)
+        .background(Color.white)
+        .clipShape(RoundedRectangle(cornerRadius: 8, style: .continuous))
+    }
+}
