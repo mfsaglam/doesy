ebd83e50e59b7e39e04533d9bbb4c5f4d563b787
mfsaglam <mfsaglam1@gmail.com>
2022-01-06 22:03:32 +0300

    Edit NewTaskButton name so can be New category too

---
+++ Doesy/View/Components/CategoriesAndTasks.swift
@@ -0,0 +1,20 @@
+//
+//  CategoriesAndTask.swift
+//  Doesy
+//
+//  Created by Fatih Sağlam on 6.01.2022.
+//
+
+import SwiftUI
+
+struct CategoriesAndTask: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CategoriesAndTask_Previews: PreviewProvider {
+    static var previews: some View {
+        CategoriesAndTask()
+    }
+}
